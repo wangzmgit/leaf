5942c6df44e7efa19b1bf6989eee80e2166eaa47
wzm <wangziming2022@163.com>
2023-01-23 10:59:42 +0800

    style: 修改审核状态变量名

--- server/domain/valid/video_valid.go
+++ server/domain/valid/video_valid.go
@@ -4,7 +4,7 @@ func ReviewStatus(role int) bool {
 	roles := map[int]string{
 		0:    "AUDIT_APPROVED",
 		2100: "WRONG_VIDEO_INFO",
-		2200: "WRONG_VIDEO_Content",
+		2200: "WRONG_VIDEO_CONTENT",
 	}
 
 	if _, ok := roles[role]; !ok {
