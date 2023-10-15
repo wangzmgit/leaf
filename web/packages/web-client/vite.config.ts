c43e0a2cabd3dd3ffc194d1b7464b50c235db585
wzm <wangziming2022@163.com>
2023-10-15 17:50:37 +0800

    fix: 处理await导致无法打包的问题

--- web/packages/web-client/vite.config.ts
+++ web/packages/web-client/vite.config.ts
@@ -1,11 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    topLevelAwait({
+      promiseExportName: "__tla",
+      promiseImportName: i => `__tla_${i}`
+    })
+  ],
   build: {
     chunkSizeWarningLimit: 2048,
     outDir: 'web', //指定输出路径
