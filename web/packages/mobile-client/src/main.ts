55aa932abca400738c305416b3d583d4df4dda30
wzm <wangziming2022@163.com>
2024-04-11 20:51:00 +0800

    fix: 暂时移除动态修改网站配置功能

--- web/packages/mobile-client/src/main.ts
+++ web/packages/mobile-client/src/main.ts
@@ -3,17 +3,17 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './stores';
 import router from './router';
-import { globalConfig } from '@leaf/utils';
+// import { globalConfig } from '@leaf/utils';
 
 // 加载网站配置
-const baseURL = globalConfig.domain ? `http${globalConfig.https ? 's' : ''}://${globalConfig.domain}` : '';
-const res = await fetch(`${baseURL}/api/config/web.json`)
-if (res.status === 200) {
-    const config = await res.json();
-    window.$title = config.title;
-    window.$icp = config.icp;
-    window.$security = config.security;
-}
+// const baseURL = globalConfig.domain ? `http${globalConfig.https ? 's' : ''}://${globalConfig.domain}` : '';
+// const res = await fetch(`${baseURL}/api/config/web.json`)
+// if (res.status === 200) {
+//     const config = await res.json();
+//     window.$title = config.title;
+//     window.$icp = config.icp;
+//     window.$security = config.security;
+// }
 
 const app = createApp(App);
 
